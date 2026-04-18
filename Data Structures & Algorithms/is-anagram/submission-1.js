6a38aef4592e03704a3916de6a9978f7a56be554
Winter <67937154+Waffle-Cone@users.noreply.github.com>
2026-04-18 10:18:07 +0100

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.js
@@ -0,0 +1,46 @@
+const check = (map)=>{
+   
+    for(let value of map.values()){
+        if(value > 0){return false}
+    }
+    return true
+}
+
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+
+        let mem = new Map()
+
+
+        for(let letter of s){
+            
+            if(mem.has(letter)){
+                mem.set(letter,mem.get(letter) + 1)
+            }else{
+                mem.set(letter,1)
+            }
+        }
+        //console.log(mem)
+        
+
+        for(let letter of t){
+            
+            if(mem.has(letter)){
+                if(mem.get(letter) > 0){
+                    mem.set(letter,mem.get(letter) - 1)
+                }else{
+                    return false
+                    }         
+            }else{
+                return false
+            }
+        }
+        
+        return check(mem)
+    }
+}
